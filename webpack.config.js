61a29cb38a3882f7e9fbe8714fdc4ebce73e8f0d
Jaime Pastor <jaime.pastor@adidas-group.com>
2021-06-02 17:03:45 +0200

    Move react-select to peer-dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
   },
   externals: {
     react: 'commonjs react',
-    'react-dom': 'commonjs react-dom'
+    'react-dom': 'commonjs react-dom',
+    'react-select': 'commonjs react-select'
   },
 };
